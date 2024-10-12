515a793a9d25e596c28fc0b9a5da48801ca5cfba
RoshniS83 <81032594+RoshniS83@users.noreply.github.com>
2024-10-12 18:52:09 +0530

    Update app.py

--- pdf_text/login/app.py
+++ pdf_text/login/app.py
@@ -184,7 +184,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'roshni.sundrani83@gmail.com'
-app.config['MAIL_PASSWORD'] = 'wutu jttw tlyn iowo'
+app.config['MAIL_PASSWORD'] = 'your app password'
 app.config['MAIL_DEFAULT_SENDER']='roshni.sundrani83@gmail.com'
 
 mail = Mail(app)
